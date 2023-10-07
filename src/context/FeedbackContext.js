82ac6cb10818933c4850b9838e031550858dced7
W <w@WdeMac-mini.lan>
2023-10-06 23:48:41 -0400

    newIcon

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -8,17 +8,18 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: "This is feedback item 1",
+      text: "It’s a great experience. The ambiance is very welcoming and charming. Amazing wines, food and service. Staff are extremely knowledgeable and make great recommendations.",
       rating: 10,
     },
     {
       id: 2,
-      text: "This is feedback item 2",
+      text: "Excellent food. Menu is extensive and seasonal to a particularly high standard. Definitely fine dining. It can be expensive but worth it and they do different deals on different nights so it’s worth checking them out before you book. Highly recommended.",
       rating: 9,
     },
     {
       id: 3,
-      text: "This is feedback item 3",
+      text: `Delicious high quality plates, selection of wines and great service.
+      We tried all their entrees, one of their desserts, and everything was de-li-cious. Highly recommended.`,
       rating: 8,
     },
   ]);
